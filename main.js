5a55f590a4c99e56a721bc1e3928bf312c858e4a
ishivyas <129364149+ishivyas@users.noreply.github.com>
2024-02-14 17:04:03 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+noseX = "";
+noseY = "";
+GameStatus = "";
+
+function startGame()
+{
+GameStatus = "start";
+document.getElementById("status").innerHTML = "Game is Loading";
+}
+
+function game()
+{
+	console.log("noseX = " + nose + " , noseY = " + noseY);
+}
+
+function preload() 
+{
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_die = loadSound("mariodie.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	
+	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+	console.log('modelLoaded');
+}
+
+function draw() 
+{
+	game()
+}
+
+function gotPoses(results)
+{
+	if(results.length > 0)
+	{
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
+}
